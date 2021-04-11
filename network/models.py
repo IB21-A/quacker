70cd83c01e80701ec349a733c221752664156723
IB21-A <8bittrio@gmail.com>
2021-04-10 17:51:02 -0700

    initial commit

---
+++ network/models.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    # posts
+    # likes
+    # following
+    
+# class Post()
+#   author
+#   body
+#   date
+#   likes
+
+
+# class Like()
+#   user
+#   post
+#   date
+
+# class Follow()
+#   Follower
+#   Followee
+#   date
+
+
