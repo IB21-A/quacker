71c982007a2307500aa63bc0fb4e6954163fd59b
Thom Zolghadr <thom@thomcodes.com>
2022-01-15 01:15:56 -0800

    Attempt fix for deployment

--- project4/settings.py
+++ project4/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 from dotenv import load_dotenv
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -173,4 +172,3 @@ LOGGING = {
 
 MEDIA_URL = '/img/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/img')
-django_heroku.settings(locals())
