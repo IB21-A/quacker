8d54c5881c870b51dc21bd9f37e1a96e1464cfca
Thom Zolghadr <thom@thomcodes.com>
2022-01-15 00:30:12 -0800

    Update gitignore Add fix for staticfiles in settings.py

--- project4/settings.py
+++ project4/settings.py
@@ -125,9 +125,10 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+# Extra places for collect static to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, '/network/static'),
+)
 
 
 MEDIA_URL = '/img/'
